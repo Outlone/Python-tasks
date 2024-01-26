d2e3694aed90c82a42a7ee45ce856256275dec4d
Kraev Ilya <114720521+Outlone@users.noreply.github.com>
2024-01-26 05:35:40 +0500

    Create task 00-13.py

---
+++ Python 0-0/task 00-13.py
@@ -0,0 +1,18 @@
+import random
+planets = ['Меркурий', 'Венера', 'Земля', 'Марс', 'Юпитер', 'Сатурн', 'Уран', 'Нептун']
+planet = random.choice(planets)
+warning = 'Вас присутствует защита от взлома!'
+planet = input()
+print (warning)
+riddle = 'Какую планету я загадал?'
+print(riddle)
+answer = input()
+if answer == 'Плутон':
+  print('Плутон уже не считается планетой.')
+elif answer not in planets:
+  print('Да это же вообще не название планеты Солнечной системы.')
+elif answer == planet:
+  print('*** Верно! *** Это', answer)
+else:
+  print('Неверно!')
+input()
