ecc28cb20807f1f1f09a505c66e1f9f7a830c886
Kraev Ilya <114720521+Outlone@users.noreply.github.com>
2024-02-20 03:59:36 +0500

    Create task 03-04.py

---
+++ Python 03/task 03-04.py
@@ -0,0 +1,8 @@
+string = input("Введите слово:")
+while len(string) != 0:
+    print(string)
+    string = input("Введите слово:")
+while len(string) == 0:
+    print(" ")
+    _ = input("Введите слово:")
+    print(" ")
