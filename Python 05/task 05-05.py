8245f6da66fadbcbfe2ee5e9b794310ffe777ef9
Kraev Ilya <114720521+Outlone@users.noreply.github.com>
2024-03-06 03:33:17 +0500

    Update task 05-05.py

--- Python 05/task 05-05.py
+++ Python 05/task 05-05.py
@@ -3,10 +3,9 @@ count = 0
 a = 0
 while words != "стоп":
     count += 1
-    words = input("Введите предложения: ")
+    words = input("Введите предложения:")
     if "Кот" in words or "кот" in words:
         a = a + count
-        break
 if a == 0:
     print("-1")
 else:
