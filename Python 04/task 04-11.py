50ad6b5e0cbd52f3d86b4d53108c58dadc1140d2
Kraev Ilya <114720521+Outlone@users.noreply.github.com>
2024-03-06 03:23:27 +0500

    Update task 04-11.py

--- Python 04/task 04-11.py
+++ Python 04/task 04-11.py
@@ -1,1 +1,13 @@
-
+ave = 0
+sum = 0
+n = int(input('Введите кол-во тестируемых людей:'))
+for a in range(n):
+    iq = int(input('Введите iq:'))
+    sum += iq
+    ave = sum/(a+1)
+    if iq == ave:
+        print('0')
+    elif iq > ave:
+        print('>')
+    else:
+        print('<')
