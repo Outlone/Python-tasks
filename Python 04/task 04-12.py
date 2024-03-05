3d3e2909ab8a78838d92934cded59ecdc12dfed1
Kraev Ilya <114720521+Outlone@users.noreply.github.com>
2024-03-06 03:25:24 +0500

    Update task 04-12.py

--- Python 04/task 04-12.py
+++ Python 04/task 04-12.py
@@ -1,1 +1,12 @@
-
+n = int(input('Введите количество натуральных чисел:'))
+if n <= 0:
+    print('Введите число, где n > 0:')
+else:
+    sum = 0
+    for a in range(n):
+        num = int(input("Введите число:"))
+        if a % 2 == 0:
+            sum += num
+        else:
+            sum -= num
+    print('Сумма знакочередующаяся ряда:', sum)
