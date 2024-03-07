1cfded276020787b7139813dd301395254adac76
Kraev Ilya <114720521+Outlone@users.noreply.github.com>
2024-03-07 06:45:16 +0500

    Create task 10-01.py

---
+++ Python 10/task 10-01.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = []
+c = 0
+for i in range(a):
+    d = int(input())
+    b.append(b)
+final = int(input())
+for i in range(a):
+    for k in range(i + 1, a):
+        if final == b[i] * b[k]:
+            f = 1
+if c == 1:
+    print("ДА")
+else:
+    print("НЕТ")
