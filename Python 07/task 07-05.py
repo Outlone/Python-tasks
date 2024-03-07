f641ca3cdd8297e60135392af77f50a6e95c76b3
Kraev Ilya <114720521+Outlone@users.noreply.github.com>
2024-03-07 06:20:59 +0500

    Update task 07-05.py

--- Python 07/task 07-05.py
+++ Python 07/task 07-05.py
@@ -1,11 +1,9 @@
-word1 = str(input("Введите слово 1: "))
-word2 = str(input("Введите слово 2: "))
+word1 = str(input("Введите первое слово:"))
+word2 = str(input("Введите второе слово:"))
 while word1[-1] == word2[0]:
-    word = str(input("Введите слово : "))
-
+    word = str(input("Введите слово :"))
     if word[-1] == word2[0]:
-        word2 = str(input("Введите новое слово : "))
-
+        word2 = str(input("Введите новое слово :"))
     else:
         print(word)
         break
